4fdafd71b9220bc74f367d776bd10fd4e2cd8a92
Cauã <cauaserpa092@gmail.com>
2025-03-27 19:54:52 -0300

    Adicionar seeder para criar usuário administrador.

---
+++ db/seeds/AdminUser.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Seed\AbstractSeed;
+
+class AdminUser extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     */
+    public function run(): void
+    {
+        // Inserir dados na tabela tb_users
+        $dataAdminUser = [
+            [
+                'role'          => 0,
+                'fisrtname'     => 'Admin',
+                'lastname'      => '001',
+                'email'         => 'admin@admin.com',
+                'text_color'    => '$2y$10$bsVxUdsFhORHKzyvcgZLWuiu8rlfRqWT9/2h6WZ.cPvYRL7ld/Z.G',
+                'active_status' => 1,
+            ]
+        ];
+        $this->table('tb_users')
+            ->insert($dataAdminUser)
+            ->save();
+ 
+    }
+}
